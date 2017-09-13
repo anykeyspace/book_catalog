83a52f2885f16b7e3ac5f71be30e6d415a2744c1
Yuri Semikov <20nomad12@mail.ru>
2017-09-13 23:02:12 +0300

    fixed classes Author and Book, add dependencies in pom.xml

--- src/main/java/anykeyspace/bookcatalog/model/Author.java
+++ src/main/java/anykeyspace/bookcatalog/model/Author.java
@@ -1,12 +1,10 @@
 package anykeyspace.bookcatalog.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Version;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
-public class Book {
+public class Author {
 
     @Id
     @GeneratedValue
@@ -18,5 +16,67 @@ public class Book {
     @Version
     private Integer version;
 
-    
+    @Basic(optional = false)
+    private String firstName;
+
+    @Basic(optional = false)
+    private String lastName;
+
+    private String country;
+
+    @OneToMany
+    private List<Book> books;
+
+    // для hibernate
+    public Author() {
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
 }
--- src/main/java/anykeyspace/bookcatalog/model/Book.java
+++ src/main/java/anykeyspace/bookcatalog/model/Book.java
@@ -1,4 +1,90 @@
 package anykeyspace.bookcatalog.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    /**
+     * Служебное поле hibernate
+     */
+    @Version
+    private Integer version;
+
+    @Basic(optional = false)
+    private String name;
+
+    private int year;
+
+    private String ganre;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Author author;
+
+    private String discription;
+
+    // для hibernate
+    public Book() {
+    }
+
+    public Book(String name, Author author) {
+        this.name = name;
+        this.author = author;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getGanre() {
+        return ganre;
+    }
+
+    public void setGanre(String ganre) {
+        this.ganre = ganre;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public String getDiscription() {
+        return discription;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
+    }
 }
